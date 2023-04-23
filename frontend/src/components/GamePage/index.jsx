8ee26c4403a208d78e5a6d75b164990e6136686d
jimcreel <jim.creel@gmail.com>
2023-04-23 09:42:02 -0700

    generating game stats

--- frontend/src/components/GamePage/index.jsx
+++ frontend/src/components/GamePage/index.jsx
@@ -7,32 +7,29 @@ export default function Game(props){
     let awayPlayers = []
 
     if(game.liveData){
-        homePlayers = game.liveData.boxscore.teams.home.players
-        awayPlayers = game.liveData.boxscore.teams.away.players
+        homePlayers = game.liveData.boxscore.teams.home
+        awayPlayers = game.liveData.boxscore.teams.away
     }
 
     let playerNames = []
     if(game.gameData){
         playerNames = game.gameData.players
     }
-    console.log(homePlayers)
-    console.log(awayPlayers)
-    console.log(playerNames)
+    
     let homeElement = <p> Loading... </p>;
     let awayElement = <p> Loading... </p>;
     let homePitcher = <p> Loading... </p>;
     let awayPitcher = <p> Loading... </p>;
 
     if(game.liveData){
-        homeElement = Object.values(homePlayers).map((player) => {
-            if(player.position.code == 'P'){
-                homePitcher = playerNames[player.person.id].fullName
-            }
-            // return(
-            //     <div className="flex flex-row justify-around items-center align-center">
-            //         <p className="text-center text-xs">{playerNames[player.person.id].fullName}</p>
-            //     </div>
-            // )
+        homeElement = Object.values(homePlayers.battingOrder).map((player) => {
+            let playerID = `ID${player}`
+            let playerGameStats = ''
+            return(
+                <div>
+                    <p>{playerGameStats}</p>
+                </div>
+            )
         })
     }   
     return(
