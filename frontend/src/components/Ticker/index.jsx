ed1e2ca0b14ffbcf546d3b9cbd6ee7174f644203
jimcreel <jim.creel@gmail.com>
2023-08-17 12:46:36 -0700

    updated logic to display ticker for no scheduled games

--- frontend/src/components/Ticker/index.jsx
+++ frontend/src/components/Ticker/index.jsx
@@ -1,42 +1,31 @@
-
+import React, { useContext, useEffect, useState } from "react";
 import GameCard from "../GameCard";
-import {useContext, useEffect} from "react";
-import {ScheduleContext} from "../Calendar";
-import {DateContext} from "../Calendar";
-
+import { ScheduleContext } from "../Calendar";
+import { DateContext } from "../Calendar";
 
 export default function Ticker(props) {
-    const schedule = useContext(ScheduleContext);
-    const {currentDate} = useContext(DateContext)
-    const {setGame} = props
+  const schedule = useContext(ScheduleContext);
+  const { currentDate } = useContext(DateContext);
+  const { setGame } = props;
 
-    useEffect(() => {
+  const [scheduleElement, setScheduleElement] = useState(<>loading...</>);
 
-    }, [schedule])
-    let scheduleElement = <> loading...</>
-    
-    if(!schedule.dates?.length > 0){
-        
-        setTimeout(() => {
-            scheduleElement = <> No games scheduled for today</>
-            }, 3000);
-        } 
-   
-    if(schedule.dates?.length > 0) {
-        scheduleElement = schedule.dates[0].games.map((gameCard) => <GameCard key={gameCard.gamePk} game={gameCard} setGame={setGame}/>)
+  useEffect(() => {
+    if (schedule.dates?.length > 0) {
+      const games = schedule.dates[0].games.map((gameCard) => (
+        <GameCard key={gameCard.gamePk} game={gameCard} setGame={setGame} />
+      ));
+      setScheduleElement(games);
+    } else {
+      setTimeout(() => {
+        setScheduleElement(<>No games scheduled for today</>);
+      }, 1000); // 4 seconds
     }
-    
+  }, [schedule, setGame]);
 
-
-
-        
-
-    return (
-    <div className="bg-white-800 flex flex-row overflow-x-auto max-h-30 mx-20">
-        
-        {scheduleElement}
-        
+  return (
+    <div className="bg-white-800 flex flex-row overflow-x-auto max-h-30 mx-20 justify-center">
+      {scheduleElement}
     </div>
-
-    );
+  );
 }
